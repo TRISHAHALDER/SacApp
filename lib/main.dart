6673f567ec49b86e74b6bbfa120a6e26440e98cf
Trisha <trishah.ug21.cs@nitp.ac.in>
2023-03-23 15:35:36 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sac_event_manager/homePage.dart';
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,5 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
